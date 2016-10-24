84f2d12185d2f99e6a5906e29f62e8a71ba68a5f
xudafeng <xudafeng@126.com>
2016-10-24 16:36:14 +0800

    update deps

--- script/build.js
+++ script/build.js
@@ -8,7 +8,7 @@ var antBuild = require('ant-build');
 antBuild({
   cwd: path.join(__dirname, '..')
 }).then(function(d) {
-  console.log(d); 
+  console.log(d);
 }).catch(function(e) {
   setTimeout(function() {
     console.log(e);
